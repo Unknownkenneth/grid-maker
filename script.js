f9db4cbcd221b90d7dffe607610133d1e933e82b
Kenneth Louis <lowkam4@gmail.com>
2022-10-23 17:35:07 -0400

    working 4/4  functions

--- script.js
+++ script.js
@@ -3,24 +3,98 @@ let numRows = 0;
 let numCols = 0;
 let colorSelected; 
 
+
 // Add a row
 function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+     let tr=document.createElement("tr"); 
+    let td=document.createElement("td");
+
+    td.addEventListener('click', () => {td.style.background = colorSelected;} ); // add color
+    
+    tr.appendChild(td); // create one column
+    for(let i =1;i<numCols;i++) // if more then one, create more rows 
+    {
+        let td1=document.createElement("td");
+       td1.addEventListener('click', () => {td1.style.background = colorSelected;} );
+        tr.appendChild(td1);
+    }
+    numRows+=1; //increase the counter for  rows 
+    if(numCols===0)//if column is 0 then increase it to one
+    {
+        numCols=1;
+    }
+    let grid=document.getElementById("grid");
+    grid.appendChild(tr); //add the row to the column
+
 }
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+    let grid=document.getElementById("grid");
+    let col=grid.querySelectorAll("tr");  // returns array of all row element(tr)
+    
+    for(let i=0;i<numRows;i++){
+
+        let td=document.createElement("td");
+        td.addEventListener('click', () => {td.style.background = colorSelected;} ); // for color on click 
+
+        col[i].appendChild(td);
+        
+
+    }
+    
+    numCols+=1;
+    if(numRows===0){
+        let tr=document.createElement("tr");
+        let td=document.createElement("td");
+        
+
+          td.addEventListener('click', () => {td.style.background = colorSelected;} );
+          
+          tr.appendChild(td);
+    
+     
+      grid.appendChild(tr); //add the row to the columnm if row and col are 0
+      numRows=1
+    }
+   
+
 }
 
 // Remove a row
 function removeR() {
-    alert("Clicked Remove Row"); // Replace this line with your code.
+    let grid= document.getElementById("grid")
+    
+    let a = grid.lastElementChild; 
+     grid.removeChild(a);    // remove one row 
+    numRows-=1
+    if(numRows===0){
+        numCols=0
+    }
+
 }
+
+
 
 // Remove a column
 function removeC() {
-    alert("Clicked Remove Col"); // Replace this line with your code.
+    
+    
+ let grid=document.getElementById('grid');
+ let cols=grid.querySelectorAll("tr") // calls all the query  
+     for(let i=0;i<numRows;i++){
+ 
+        let last= cols[i].lastElementChild;
+        cols[i].removeChild(last);   // removes the last columns
+   } 
+numCols-=1 // sub by one 
+  if(numCols===0){
+    numRows=0
+  }
+
+  
+
+
 }
 
 // Set global variable for selected color
@@ -30,16 +104,42 @@ function selectColor(){
 }
 
 // Fill all uncolored cells
-function fillU(){
-    alert("Clicked Fill All Uncolored"); // Replace this line with your code.
+function fillU(){//if color is blank then change it
+    
+    let td = grid.querySelectorAll("td");
+    td.forEach(td=>{
+        if(td.style.background=== ""){  
+            td.style.background= colorSelected;
+        }
+    })
 }
 
 // Fill all cells
 function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
+    let grid =document.getElementById("grid");
+   let td =grid.querySelectorAll("td");
+   td.forEach(td =>{
+    td.style.background= colorSelected;
+   })
+
+     
+   //if( td.style.background != colorSelected){   // alert("Clicked Fill All")
+    
+
+  //  td.style.backgroundColor= colorSelected
+   //}
+   // Replace this line with your code. // might need all query elements once doing more research
+
 }
 
-// Clear all cells
+// Clear all cells // means remove all colors?
 function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
+    
+    let grid =document.getElementById("grid");
+    let td = grid.querySelectorAll("td");
+    
+  td.forEach(tr => {
+    removeR() //  this has an eqaul col to row match inside the function
+  })
+    
 }
